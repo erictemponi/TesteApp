71924dc07604100f1eb9ec04da5f601b8c954639
Erictemponi <erictemponi_vdl@hotmail.com>
2021-09-14 17:59:13 -0300

    Adicionar API RESTFUL

---
+++ Api/Controllers/TodoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Services;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        public TodoController() { }
+
+        // Obter todas as tarefas
+        [HttpGet]
+        public ActionResult<List<Todo>> ObterTodas() => TodoService.ObterTodas();
+
+        // Obter todas as tarefas concluídas
+        [HttpGet("{done}")]
+        public ActionResult<List<Todo>> ObterTodasConcluidas(string done)
+        {
+            if (!(done.ToLower() == "done"))
+                return BadRequest();
+            return TodoService.ObterTodasConcluidas();
+        }
+
+        
+        // Obter todas uma tarefa específica
+        [HttpGet("{id:int}")]
+        public ActionResult<Todo> Obter(int id)
+        {
+            var todo = TodoService.Obter(id);
+
+            if (todo == null)
+                return NotFound();
+
+            return todo;
+        }
+        
+
+        [HttpPost]
+        public IActionResult Create(Todo todo)
+        {
+            TodoService.Inserir(todo);
+            return CreatedAtAction(nameof(Create), new { id = todo.Id }, todo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Todo todo)
+        {
+            if (id != todo.Id)
+                return BadRequest();
+
+            var existingTodo = TodoService.Obter(id);
+            if (existingTodo is null)
+                return NotFound();
+
+            TodoService.Alterar(todo);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todo = TodoService.Obter(id);
+
+            if (todo is null)
+                return NotFound();
+
+            TodoService.Remover(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Api/Services/TodoService.cs
@@ -0,0 +1,45 @@
+﻿using Api.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Services
+{
+    public class TodoService
+    {
+        static List<Todo> Todos { get; }
+        static int nextId = 4;
+        static TodoService()
+        {
+            Todos = new List<Todo>
+            {
+                new Todo { Id = 1, Title = "Tarefa 01" },
+                new Todo { Id = 2, Title = "Tarefa 02", IsDone = true },
+                new Todo { Id = 3, Title = "Tarefa 03" }
+            };
+        }
+
+        public static List<Todo> ObterTodas() => Todos;
+        public static List<Todo> ObterTodasConcluidas() => Todos.FindAll(t => t.IsDone == true);
+        public static Todo Obter(int id) => Todos.FirstOrDefault(t => t.Id == id);
+        public static void Inserir(Todo todo)
+        {
+            todo.Id = nextId++;
+            Todos.Add(todo);
+        }
+        public static void Remover(int id)
+        {
+            var todo = Todos.Find(t => t.Id == id);
+            if (todo is null)
+                return;
+            Todos.Remove(todo);
+        }
+
+        public static void Alterar(Todo todo)
+        {
+            var index = Todos.FindIndex(t => t.Id == todo.Id);
+            if (index == -1)
+                return;
+            Todos[index] = todo;
+        }
+    }
+}
