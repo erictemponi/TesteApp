c049d3af21cda2655ad10b6e2f8c7e601dbea242
Erictemponi <erictemponi_vdl@hotmail.com>
2021-09-15 01:01:50 -0300

    Iniciar documentação com Swagger

--- Api/Controllers/TodoController.cs
+++ Api/Controllers/TodoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,37 @@ namespace Api.Controllers
     {
         public TodoController() { }
 
-        // Obter todas as tarefas
-        [HttpGet]
+        /// <summary>
+        /// Retorna todas as tarefas da lista.
+        /// </summary>
+        /// <returns>Uma lista com todas as tarefas.</returns>
+        /// <remarks>
+        /// Requisição simples:
+        /// 
+        ///     Get /todo
+        ///     
+        /// </remarks>
+        /// <response code="200">Retorna uma lista com todas as tarefas.</response>
+        [HttpGet(Name = nameof(ObterTodas))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<List<Todo>> ObterTodas() => TodoService.ObterTodas();
 
-        // Obter todas as tarefas concluídas
-        [HttpGet("{done}")]
+        /// <summary>
+        /// Retorna todas as tarefas da lista marcadas como concluídas
+        /// </summary>
+        /// <param name="done">Precisa digitar done depois do get</param>
+        /// <returns>Uma lista com todas as tarefas concluídas</returns>
+        /// <remarks>
+        /// Requisição simples:
+        /// 
+        ///     Get /todo/done
+        ///     
+        /// </remarks>
+        /// <response code="200">Retorna uma lista com todas as tarefas concluídas</response>
+        [HttpGet("{done}", Name = nameof(ObterTodasConcluidas))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<List<Todo>> ObterTodasConcluidas(string done)
         {
             if (!(done.ToLower() == "done"))
@@ -27,8 +53,23 @@ namespace Api.Controllers
             return TodoService.ObterTodasConcluidas();
         }
         
-        // Obter todas uma tarefa específica
-        [HttpGet("{id:int}")]
+        /// <summary>
+        /// Retorna uma tarefa específica com base em um ID
+        /// </summary>
+        /// <param name="id">Idenficador da tarefa. É um número inteiro</param>
+        /// <returns>A tarefa de ID igual ao ID passado como parâmetro</returns>
+        /// <remarks>
+        /// Requisição simples:
+        /// 
+        ///     Get /todo/1
+        ///     
+        /// </remarks>
+        /// <response code="200">Retorna a tarefa cujo ID é igual ao ID passado como parâmetro</response>
+        /// <response code="404">Não existe uma tarefa na lista com o ID passado por parâmetro</response>
+        [HttpGet("{id:int}", Name = nameof(Obter))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Todo> Obter(int id)
         {
             var todo = TodoService.Obter(id);
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace Api
 {
@@ -30,18 +32,35 @@ namespace Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Tarefas",
+                    Version = "v1",
+                    Description = "Manipula tarefas a serem realizadas",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Eric Temponi",
+                        Email = "erictemponi@hotmail.com",
+                        Url = new Uri("https://github.com/erictemponi")
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
             }
 
             app.UseHttpsRedirection();
