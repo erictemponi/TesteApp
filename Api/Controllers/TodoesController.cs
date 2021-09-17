15aacd817164f6a740a20e8df1a00faaa42ac46d
Erictemponi <erictemponi_vdl@hotmail.com>
2021-09-16 22:02:03 -0300

    Adicionar documentação Swagger à controladora da base de dados

--- Api/Controllers/TodoesController.cs
+++ Api/Controllers/TodoesController.cs
@@ -22,14 +22,43 @@ namespace Api.Controllers
         }
 
         // GET: api/Todoes
-        [HttpGet]
+        /// <summary>
+        /// Retorna todas as tarefas da base de dados.
+        /// </summary>
+        /// <returns>Uma lista com todas as tarefas.</returns>
+        /// <remarks>
+        /// Requisição simples:
+        /// 
+        ///     Get /todoes
+        ///     
+        /// </remarks>
+        /// <response code="200">Retorna uma lista com todas as tarefas.</response>
+        [HttpGet(Name = nameof(GetTodos))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
         {
             return await _context.Todos.ToListAsync();
         }
 
         // GET: api/Todoes/5
-        [HttpGet("{id}")]
+        /// <summary>
+        /// Retorna uma tarefa específica com base em um ID
+        /// </summary>
+        /// <param name="id">Idenficador da tarefa. É um número inteiro</param>
+        /// <returns>A tarefa de ID igual ao ID passado como parâmetro</returns>
+        /// <remarks>
+        /// Requisição simples:
+        /// 
+        ///     Get /todoes/1
+        ///     
+        /// </remarks>
+        /// <response code="200">Retorna a tarefa cujo ID é igual ao ID passado como parâmetro</response>
+        /// <response code="404">Não existe uma tarefa na lista com o ID passado por parâmetro</response>
+        [HttpGet("{id:int}", Name = nameof(GetTodo))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Todo>> GetTodo(int id)
         {
             var todo = await _context.Todos.FindAsync(id);
@@ -44,7 +73,26 @@ namespace Api.Controllers
 
         // PUT: api/Todoes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        /// <summary>
+        /// Altera uma tarefa existente na base de dados de tarefas
+        /// </summary>
+        /// <param name="id">ID da tarefa a ser alterada</param>
+        /// <param name="todo">A tarefa alterada</param>
+        /// <returns>Retorna uma mensagem de NoContent (sem conteúdo) no caso da alteração ser realizada com sucesso</returns>
+        /// <remarks>
+        /// Alteração simples:
+        /// 
+        ///     Put 4 -c "{"id": 4, "title":"Novo título", "isDone": false}"
+        ///     
+        /// </remarks>
+        /// <response code="204">Tarefa alterada com sucesso</response>
+        /// <response code="400">Ocorre quando o id depois do Put for diferente do id da tarefa</response>
+        /// <response code="404">Ocorre quando não existe uma tarefa com o id passado como parâmetro</response>
+        [HttpPut("{id}", Name = nameof(PutTodo))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> PutTodo(int id, Todo todo)
         {
             if (id != todo.Id)
@@ -75,7 +123,21 @@ namespace Api.Controllers
 
         // POST: api/Todoes
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        /// <summary>
+        /// Insere uma nova tarefa na base de dados
+        /// </summary>
+        /// <param name="todo">Tarefa a ser adicionada na lista</param>
+        /// <returns>A nova tarefa adicionada</returns>
+        /// <remarks>
+        /// Inserção simples:
+        /// 
+        ///     Post -c "{"title":"Nova tarefa"}"
+        ///     
+        /// </remarks>
+        /// <response code="201">Adiciona a nova tarefa na base de dados e a retorna</response>
+        [HttpPost(Name = nameof(PostTodo))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<ActionResult<Todo>> PostTodo(Todo todo)
         {
             _context.Todos.Add(todo);
@@ -85,7 +147,23 @@ namespace Api.Controllers
         }
 
         // DELETE: api/Todoes/5
-        [HttpDelete("{id}")]
+        /// <summary>
+        /// Remove uma tarefa da base de dados de tarefas
+        /// </summary>
+        /// <param name="id">Identificador da tarefa a ser removida</param>
+        /// <returns>Retorna uma mensagem de NoContent (sem conteúdo) no caso da remoção ser realizada com sucesso</returns>
+        /// <remarks>
+        /// Remoção simples
+        /// 
+        ///     Delete 2
+        ///     
+        /// </remarks>
+        /// <response code="204">Tarefa removida com sucesso</response>
+        /// <response code="404">Ocorre quando não existe uma tarefa com o id passado como parâmatro</response>
+        [HttpDelete("{id}", Name = nameof(DeleteTodo))]
+        [Produces("text/json")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteTodo(int id)
         {
             var todo = await _context.Todos.FindAsync(id);
